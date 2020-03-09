fc77fefe7efa231aa36fa9fb704ed316e1ba3b92
spdobest <sp.dobest@gmail.com>
2020-03-09 19:38:25 +0000

    Renamed project, starting from scratch

---
+++ src/main/java/spm/spring/world/SpringBootWorldApplication.java
@@ -0,0 +1,13 @@
+package spm.spring.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spm/spring/world/SpringBootWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package spm.spring.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
