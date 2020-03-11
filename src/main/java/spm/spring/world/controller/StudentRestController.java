ea6476ff5dd25ba3b44cbd8e22762221f27a2fa4
Sibaprasad <sibaprasad.X.mohanty@aib.ie>
2020-03-11 16:09:52 +0000

    added different controller to do different operation like webservices and webpage operations

--- src/main/java/spm/spring/world/controller/StudentRestController.java
+++ src/main/java/spm/spring/world/controller/StudentRestController.java
@@ -3,27 +3,21 @@ package spm.spring.world.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import spm.spring.world.model.Student;
 import spm.spring.world.response.ResponseModel;
 import spm.spring.world.service.StudentService;
 
 import javax.validation.Valid;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 
 /**
- * StudentController class is for webpage related operation and StudentRestController is for web services calls
+ * THis class is for web services calls and StudentController is for webpage related operation
  */
 
-//@RestController    // Useful to create the RESTful webservices.
-@Controller          // usefulll to create usefull webpages
-public class StudentController {
+@RestController    // Useful to create the RESTful webservices.
+public class StudentRestController {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
@@ -35,7 +29,7 @@ public class StudentController {
     // @PostMapping annotation handles the http post request matched with the given uri.
     // @RequestBody annotation binds the http request body to the domain object.
     // @Valid annotation validates a model after binding the user input to it.
-    @PostMapping(value = "/student/save")
+    @PostMapping(value = "/student/saveStudent")
     public int save(final @RequestBody @Valid Student student) {
         log.info("Saving student details in the database.");
         service.save(student);
@@ -44,20 +38,14 @@ public class StudentController {
 
     // Get all students from the h2 database.
     // @GetMapping annotation handles the http get request matched with the given uri.
-    @GetMapping(value = "/student/getall", produces = "application/vnd.jcg.api.v1+json")
+    @GetMapping(value = "/student/getallStudent", produces = "application/vnd.jcg.api.v1+json")
     public List<Student> getAll() {
         log.info("Getting student details from the database.");
         return service.getAll();
     }
 
-    // FOR FORM
-    @RequestMapping("/addStudent")
-    public String formGet(Model model) {
-        return "addOrUpdateStudent";
-    }
 
-
-    @PostMapping(path = "/addUpdateStudent", consumes = "application/json", produces = "application/json")
+    @PostMapping(path = "/insertStudent", consumes = "application/json", produces = "application/json")
     @ResponseBody
     public ResponseModel formPost(@RequestBody Student student, Model model) {
         model.addAttribute("student", student);
@@ -74,17 +62,8 @@ public class StudentController {
         return responseModel;
     }
 
-
-    @RequestMapping("/students")
-    public ModelAndView showStudents() {
-
-        List<Student> students = service.getAll();
-        System.out.println("inside cities " + students.size());
-        Map<String, Object> params = new HashMap<>();
-
-        params.put("students", students);
-        System.out.println("inside cities ");
-
-        return new ModelAndView("showStudents", params);
+    @RequestMapping("/allStudents")
+    public List<Student> getAllStudents() {
+        return service.getAll();
     }
 }
