5141a4123f87a102cb4f81ea551e3a8965a37428
spdobest <sp.dobest@gmail.com>
2020-04-18 20:43:42 +0100

    First Commit

--- src/main/java/spm/spring/world/SpringBootIntroApplication.java
+++ src/main/java/spm/spring/world/SpringBootIntroApplication.java
@@ -2,13 +2,10 @@ package spm.spring.world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 
 @SpringBootApplication
-@EnableScheduling
-public class MySqlWithJpaSecurityApplication {
+public class SpringBootIntroApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MySqlWithJpaSecurityApplication.class, args);
+        SpringApplication.run(SpringBootIntroApplication.class, args);
     }
 }
